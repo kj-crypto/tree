e8bcabb2e6613d3c0411dd96708f9650fa713f9e
konrad <kjawulski@wp.pl>
2025-06-11 06:11:29 +0200

    Init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Tree
+Tree is a simple tree implementation for Go.
+
+## Build
+```
+go build -o tree -ldflags "-s -w" tree.go
+```
---
+++ tree.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/fs"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/charmbracelet/gum/pager"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/lipgloss/tree"
+)
+
+var (
+	enumeratorStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#353ec5")).MarginRight(1)
+	rootStyle       = lipgloss.NewStyle().Foreground(lipgloss.Color("#0cff04")).Bold(true)
+	itemStyle       = lipgloss.NewStyle().Foreground(lipgloss.Color("#ffbbe8"))
+)
+
+func getTree(root string, maxDepth int) *tree.Tree {
+	t := tree.New()
+	last_node := make(map[int]*tree.Tree)
+	last_node[0] = t
+
+	err := filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if path == root {
+			return nil
+		}
+
+		depth := strings.Count(path, string(os.PathSeparator)) - strings.Count(root, string(os.PathSeparator))
+		name := filepath.Base(path)
+		if maxDepth > 0 && depth > maxDepth {
+			if d.IsDir() {
+				return fs.SkipDir
+			}
+			return nil
+		}
+
+		if d.IsDir() {
+			t_new := tree.Root(rootStyle.Render(name))
+			last_node[depth].Child(t_new)
+			last_node[depth+1] = t_new
+		} else {
+			last_node[depth].Child(itemStyle.Render(name))
+		}
+		return nil
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return t
+}
+
+func argParse() (string, int) {
+	var maxDepth int
+	var root string
+
+	flag.Usage = func() {
+		fmt.Println(`Usage: tree [options] dir
+
+dir is the root directory. Default is "."`)
+		flag.PrintDefaults()
+	}
+	flag.IntVar(&maxDepth, "depth", 0, "Maximum depth to display")
+	flag.IntVar(&maxDepth, "d", 0, "Maximum depth to display")
+	flag.Parse()
+
+	root = "."
+	if flag.NArg() > 0 {
+		root = flag.Arg(0)
+	}
+
+	return root, maxDepth
+}
+
+func main() {
+	root, maxDepth := argParse()
+	t := getTree(root, maxDepth).
+		Enumerator(tree.RoundedEnumerator).
+		EnumeratorStyle(enumeratorStyle)
+
+	opts := pager.Options{
+		Content: t.String(),
+	}
+	if err := opts.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
